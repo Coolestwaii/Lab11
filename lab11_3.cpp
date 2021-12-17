a3e8ebd7674d82ed7833970eb8306c34da134882
waii <yougle55567@gmail.com>
2021-12-17 16:11:14 +0700

    lab11 done

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,24 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main (){
+    int i = 0;
+    float sum = 0, avg, sdsum = 0;
+    string text;
+    ifstream source("score.txt");
+    while(getline(source, text)){
+        sum += atof(text.c_str());
+        sdsum += pow(atof(text.c_str()), 2);
+        i++;
+    }
+    avg = sum/i;
+
+    cout << "Number of data = " << i << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << avg << "\n";
+    cout << "Standard deviation = " << (sqrt((sdsum/i) - pow(avg, 2)));
+}
